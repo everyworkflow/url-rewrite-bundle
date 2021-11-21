67b30f41c62bb498b150e4f48781984255e6d4d7
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:00:03 +0000

    Added ?ExtensionInterface

--- EveryWorkflowUrlRewriteBundle.php
+++ EveryWorkflowUrlRewriteBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\UrlRewriteBundle;
 
 use EveryWorkflow\UrlRewriteBundle\DependencyInjection\UrlRewriteExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowUrlRewriteBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new UrlRewriteExtension();
     }
